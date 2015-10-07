8692dc2ee11b824aaef99ee5c30fc7cdfc3aacb7
Michael T. Mordowanec <michael.mordowanec@gmail.com>
2015-10-07 15:32:30 -0400

    SECRET_KEY now set as environment variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '11w=doh5ji0$sx$k5b1(m^q2t7g%*63w5_nqz8%6g#ms3_ju^a'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
